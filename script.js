f4eb2ce0148b85108003e6993ecd1bdfb122220f
Clayell <125416952+Clayell@users.noreply.github.com>
2025-12-04 22:09:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Automatically removes YouTube's adblock enforcement popup
-// @author       You
+// @author       Clayel
 // @match        *://www.youtube.com/*
 // @grant        none
 // ==/UserScript==
